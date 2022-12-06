9fa99de0cb5053b796c533944d36150445bf50cd
sofiaaaalva <118946704+sofiaaaalva@users.noreply.github.com>
2022-12-06 15:31:29 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,7 @@ $(document).ready(function () {
     $('#smartcart').smartCart({
 
         cartItemTemplate: '<div class="d-flex">'+
+
             '<h3 class="list-group-item-heading">{product_name}</h3>' +
         '</div>',
 
